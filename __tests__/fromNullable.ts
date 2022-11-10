60ca7c3a172d81965a2d378625fd8eea3cddfaa8
Qimat Luo <qimatluo@westpharma.com>
2022-11-10 18:53:01 +0800

    npm run format

--- __tests__/fromNullable.ts
+++ __tests__/fromNullable.ts
@@ -1,18 +1,18 @@
-import { TestScheduler } from 'rxjs/testing';
-import { throttleTime } from 'rxjs';
+import { TestScheduler } from "rxjs/testing";
+import { throttleTime } from "rxjs";
 
 const testScheduler = new TestScheduler((actual, expected) => {
   expect(actual).toEqual(expected);
 });
 
 // This test runs synchronously.
-it('generates the stream correctly', () => {
+it("generates the stream correctly", () => {
   testScheduler.run((helpers) => {
     const { cold, time, expectObservable, expectSubscriptions } = helpers;
-    const e1 = cold(' -a--b--c---|');
-    const e1subs = '  ^----------!';
-    const t = time('   ---|       '); // t = 3
-    const expected = '-a-----c---|';
+    const e1 = cold(" -a--b--c---|");
+    const e1subs = "  ^----------!";
+    const t = time("   ---|       "); // t = 3
+    const expected = "-a-----c---|";
 
     expectObservable(e1.pipe(throttleTime(t))).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
